1d45e4e7320fcee726cc2ca42c16a3235972ab89
Casio25 <mishakolomiets355@gmail.com>
2025-10-03 20:17:33 +0300

    statix export fix for static site

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", //static export
+  images: {
+    unoptimized: true, //для статического экспорта нужно отключить оптимизацию изображений
+  }
 };
 
 export default nextConfig;
